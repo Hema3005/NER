0e045bda0840e2b59ecb3919473327d7e5355781
Hema3005 <66409440+Hema3005@users.noreply.github.com>
2020-07-05 21:56:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,jsonify
 import pos_tagger  #create module 
 import json
-
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__,template_folder="template")
+
+file_handler=RotatingFileHandler("error.log",maxBytes= 1024 * 1024 *100)
+app.logger.addHandler(file_handler)
+
 
 @app.route('/')
 def index():
@@ -11,24 +15,26 @@ def index():
 
 @app.route('/about')
 def about():
-	return "<h1>Name entity Recognition using spacy"
+	return "<h1>Name entity Recognizer<h1> <p>This is the  service to used to extract entity from entered url and list out based on the entity and name list <p>"
 
-def get_entity_namelist(json_data):
-    results=[]
-    for items in json_data:
-      results.append({items.get("Entity"):items.get("Name")})
-    return results
+@app.route("/api/get_entity", methods=['GET'])
+def get_entity():
+  url=request.args.get("url")
+  content = pos_tagger.send(url)
+  
+  return jsonify(content)
 
 
+@app.errorhandler(500)
+def handle_500_error(exception):
+    app.logger.error(exception)
+    return "Internal Server Error"
 
-@app.route("/process",methods =['POST', 'GET'])
-def process():
-    if request.method == 'POST':
-        url = request.form['url']
-        json_data=pos_tagger.send(url)         
-        results=get_entity_namelist(json_data)  
-        print("running succesfully!!!")  
-        return render_template("index.html",results=results)
+@app.errorhandler(404)
+def handle_404_error(exception):
+    app.logger.error(exception)
+    return "Not Found"
+
 
 if __name__ == '__main__':
 	app.run(debug=True,port=5003)
